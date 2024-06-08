cca9f9636a2a7dfdf8b4d0c98708781edf08be43
kinhosz <jcsc@odoo.com>
2024-06-08 15:12:59 +0200

    [FEAT] signup: add client page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,19 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        wallet_blue: '#344960',
+        wallet_orange: '#F46524',
+        wallet_orange_light: '#FF7F50',
+        wallet_orange_dark: '#E65100',
+        wallet_red: '#C53928',
+      },
+      fontFamily: {
+        wallet_primary: ['Inter', 'sans-serif'],
+        wallet_secondary: ['Helvetica Neue', 'Arial', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
