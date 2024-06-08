9e4e03a21905bea0ee4d00f99edff416aac4fc8c
kinhosz <jcsc@odoo.com>
2024-06-08 15:12:59 +0200

    [FEAT] signup: add client page

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,5 +1,7 @@
 import { useFetcher } from '@remix-run/react';
 import { ActionFunctionArgs, json, redirect } from '@remix-run/node';
+import React, { useState } from 'react';
+import { CiWarning } from 'react-icons/ci';
 
 export const action = async ({
     request,
@@ -19,24 +21,110 @@ export const action = async ({
         if (response.ok) {
             return redirect('/');
         } else {
-            return json({}, { status: response.status });
+            const data = await response.json();
+            const errorMessage = data.status ? data.status.message : 'Unknown error';
+            return json({ error: errorMessage });
         }
     } catch(error) {
         return redirect('/error');
     }
 }
 
+const WarningField: React.FC<{
+    condition: boolean;
+    message: string;
+}> = ({
+    condition,
+    message
+}) => {
+    if (!condition) return null;
+    return (
+        <div className='font-wallet_secondary text-white text-sm flex items-center'>
+            <CiWarning className="mx-2" />
+            <span>{message}</span>
+        </div>
+    );
+}
+
 function Signup() {
     const fetcher = useFetcher();
+    const [isEmailOnFocus, setIsEmailOnFocus] = useState(false);
+    const [isValidEmail, setIsValidEmail] = useState(false);
+    const [isPasswordOnFocus, setIsPasswordOnFocus] = useState(false);
+    const [atLeast6Characters, setAtLeast6Characters] = useState(false);
+    const [isNameOnFocus, setIsNameOnFocus] = useState(false);
+    const [isValidName, setIsValidName] = useState(false);
+
+    const validateEmail = (email: string) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    const handleEmailChange = (email: string) => {
+        setIsValidEmail(validateEmail(email));
+    };
+    const handlePasswordChange = (password: string) => {
+        setAtLeast6Characters(password.length >= 6);
+    };
+    const handleNameChange = (name: string) => {
+        setIsValidName(name.length != 0);
+    }
 
     return (
-        <div>
-            <h1>Signup</h1>
-            <fetcher.Form method="post" action="/signup">
-                <input name="email" type="email" placeholder="Email" />
-                <input name="password" type="password" placeholder="Password" />
-                <button type="submit">Submit</button>
-            </fetcher.Form>
+        <div className='bg-wallet_blue w-screen h-screen flex items-center justify-center font-wallet_primary'>
+            <div className='flex flex-col space-y-8 md:w-[calc(100%/5)] min-w-16'>
+                <h1 className='text-4xl font-bold text-white text-center'>Signup</h1>
+                <fetcher.Form method="post" action="/signup" className='flex flex-col space-y-4'>
+                    <input
+                        className={`rounded-xl text-sm p-2 w-full placeholder-text-base ${
+                                !isValidEmail ? 'focus:border-wallet_red focus:border-2 focus:outline-none' : ''
+                            }
+                        `}
+                        name="name" type="text" placeholder="Name" onChange={(e) => handleNameChange(e.target.value)}
+                        onFocus={() => setIsNameOnFocus(true)} onBlur={() => setIsNameOnFocus(false)}
+                    />
+                    <WarningField
+                        condition={!isValidName && isNameOnFocus}
+                        message="Empty name"
+                    />
+                    <input
+                        className={`rounded-xl text-sm p-2 w-full placeholder-text-base ${
+                                !isValidEmail ? 'focus:border-wallet_red focus:border-2 focus:outline-none' : ''
+                            }
+                        `}
+                        name="email" type="email" placeholder="Email" onChange={(e) => handleEmailChange(e.target.value)}
+                        onFocus={() => setIsEmailOnFocus(true)} onBlur={() => setIsEmailOnFocus(false)}
+                    />
+                    <WarningField
+                        condition={!isValidEmail && isEmailOnFocus}
+                        message="Invalid email"
+                    />
+                    <input
+                        className={`rounded-xl text-sm p-2 w-full placeholder-text-base ${
+                                !atLeast6Characters ? 'focus:border-wallet_red focus:border-2 focus:outline-none' : ''
+                            }
+                        `}
+                        name="password" type="password" placeholder="Password" onChange={(e) => handlePasswordChange(e.target.value)}
+                        onFocus={() => setIsPasswordOnFocus(true)} onBlur={() => setIsPasswordOnFocus(false)}
+                    />
+                    <WarningField
+                        condition={!atLeast6Characters && isPasswordOnFocus}
+                        message="Minimum is 6 characters"
+                    />
+                    <div className='flex justify-center'>
+                        <button 
+                            className={`bg-wallet_orange rounded-xl text-bold text-sm text-white py-2 px-8 button
+                                        hover:bg-wallet_orange_dark active:bg-wallet_orange_light focus:ring-2 
+                                        focus:ring-wallet_orange focus:ring-opacity-50 transition duration-150 ease-in-out ${
+                                    !(isValidEmail && atLeast6Characters && isValidName) ? 'button-disabled' : ''
+                                }
+                            `}
+                            type="submit" disabled={!isValidEmail}
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </fetcher.Form>
+            </div>
         </div>
     );
 }
