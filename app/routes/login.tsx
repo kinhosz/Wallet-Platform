d0a0d7278eb7466b185ff2f6eb2fde92582d0b0c
laysearaujo <lrsa@cin.ufpe.br>
2024-06-26 17:25:56 -0300

    update: login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,54 +1,60 @@
-import React, { useState } from 'react';
+import { ActionFunctionArgs, json, redirect } from '@remix-run/node';
+import { useActionData, Form } from '@remix-run/react';
 import WarningField from '../components/warning';
 
-function Login() {
-    const [error, setError] = useState<string | null>(null);
+export const action = async ({ 
+    request 
+}: ActionFunctionArgs) => {
+    const formData = await request.formData();
+    const user = Object.fromEntries(formData);
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    try {
+        const response = await fetch('http://localhost:3001/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ user: user }),
+        });
 
-        const formData = new FormData(event.currentTarget);
-        const user = {
-            email: formData.get('email') as string,
-            password: formData.get('password') as string,
-        };
-
-        try {
-            const response = await fetch('http://localhost:3001/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ user }),
-            });
-
-            if (response.ok) {
-                window.location.href = '/'; 
-            } else if (response.status === 401) {
-                setError('Invalid email or password.');
+        if (response.ok) {
+            const token = response.headers.get('Authorization');
+            if (token) {
+                return redirect('/', {
+                    headers: {
+                        'Set-Cookie': `token=${token}; Path=/; HttpOnly`,
+                    },
+                });
             } else {
-                setError('Failed to login. Please try again later.');
+                return json({ error: 'Failed to retrieve token.' }, { status: 401 });
             }
-        } catch (error) {
-            console.error('Error:', error);
-            setError('Failed to login. Please try again later.');
+        } else {
+            const errorMessage = response.status === 401 ? 'Invalid Email Or Password' : 'Unknow Error';
+
+            return json({ error: errorMessage }, { status: response.status });
         }
-    };
+    } catch (error) {
+        return json({ error: 'An unexpected error occurred. Please try again later.' }, { status: 500 });
+    }
+};
+
+function Login() {
+    const actionData = useActionData<typeof action>();
 
     return (
         <div className='bg-wallet_blue w-screen h-screen flex items-center justify-center font-wallet_primary'>
             <div className='flex flex-col space-y-8 md:w-[calc(100%/5)] min-w-16'>
                 <h1 className='text-4xl font-bold text-white text-center'>Login</h1>
-                <form onSubmit={handleSubmit} className='flex flex-col space-y-4'>
-                    <input name="email" type="email" placeholder="Email" className='rounded-xl text-sm p-2 w-full placeholder-text-base'/>
-                    <input name="password" type="password" placeholder="Password" className='rounded-xl text-sm p-2 w-full placeholder-text-base'/>
-                    <WarningField condition={!!error} message={error || ''} />
+                <Form method="post" className='flex flex-col space-y-4'>
+                    <input name="email" type="email" placeholder="Email" className='rounded-xl text-sm p-2 w-full placeholder-text-base' />
+                    <input name="password" type="password" placeholder="Password" className='rounded-xl text-sm p-2 w-full placeholder-text-base' />
+                    <WarningField condition={!!actionData?.error} message={actionData?.error || ''} />
                     <div className='flex justify-center'>
                         <button type="submit" className='bg-wallet_orange rounded-xl text-bold text-sm text-white py-2 px-8'>
                             Submit
                         </button>
                     </div>
-                </form>
+                </Form>
             </div>
         </div>
     );
