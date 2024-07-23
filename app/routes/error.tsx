f18b6f0ecd0958a6e598ab5f05398771f016a508
SinhoSZ <jailsonjosedc@gmail.com>
2024-07-23 18:30:49 -0300

    iniciando o trabalho

--- app/routes/error.tsx
+++ app/routes/error.tsx
@@ -1,7 +1,13 @@
 function InternalError() {
     return (
-        <div>
-            <h2>mudanças</h2>
+        <div className='bg-wallet_blue w-screen h-screen flex items-center justify-center font-wallet_primary'>
+            <div 
+                className='bg-orange-500 p-8 flex flex-col items-center justify-center rounded-lg'
+                style={{ width: '600px', height: '200px' }}
+            >
+                <h2 className="text-white text-xl text-center">Wallet is offline</h2>
+                <h3 className="text-white text-lg text-center">Take a coffee</h3>
+            </div>
         </div>
     );
 }
