db00354439c2858e48cf1bf715d1bae1d0f4aa31
laysearaujo <lrsa@cin.ufpe.br>
2024-08-01 11:42:45 -0300

    add: componentize sidebar

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -7,7 +7,8 @@ import TabSheet from "~/components/tabsheet";
 import { BR } from "country-flag-icons/react/3x2";
 import { MdChevronRight } from "react-icons/md";
 import { useEffect, useState } from "react";
-import { FiUser, FiLogOut } from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
+import Sidebar from "../components/sidebar";
 
 export async function loader({ request }: ActionFunctionArgs) {
   const token = await GetAuthToken(request);
@@ -80,29 +81,11 @@ export default function Index() {
           <TabSheet active={activePath == 'charts'} title={'Charts'} to={'/charts'} />
         </div>
       </nav>
-      {isSidebarOpen && (
-        <>
-          <button
-            className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-40"
-            onClick={toggleSidebar}
-          ></button>
-          <div className="fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-50 flex flex-col justify-between">
-            <div className="p-4">
-              <h2 className="text-xl font-bold text-blue-600">Hello, {context.user.name}!</h2>
-              <ul className="mt-4">
-                <li className="py-2">
-                  <a href="/settings" className="font-normal">Settings and Privacy</a>
-                </li>
-              </ul>
-            </div>
-            <a href="/logout" className="p-4 flex items-center justify-between cursor-pointer hover:bg-gray-100">
-              <span className="text-red-600">Logout</span>
-              <FiLogOut className="text-red-600" size={20} />
-            </a>
-            <button onClick={toggleSidebar} className="absolute top-4 right-4 text-xl">X</button>
-          </div>
-        </>
-      )}
+      <Sidebar
+        isOpen={isSidebarOpen}
+        toggleSidebar={toggleSidebar}
+        userName={context.user.name}
+      />
       <main className={isSidebarOpen ? 'filter blur-sm' : ''}>
         <Outlet />
       </main>
