d1bc24332309b6dc59796a9a48f9571be6a742af
Jailson José <167463538+SinhoSZ@users.noreply.github.com>
2024-08-04 19:35:09 -0300

    feat/error (#12)
    * add code
    
    * iniciando o trabalho
    
    * mudei a fonte do nome, confere se está bom
    
    * [fix] error: negrito removido

--- app/routes/error.tsx
+++ app/routes/error.tsx
@@ -1,7 +1,13 @@
 function InternalError() {
     return (
-        <div>
-            <h2>Internal Error</h2>
+        <div className='bg-wallet_blue w-screen h-screen flex items-center justify-center font-wallet_primary'>
+            <div 
+                className='bg-orange-500 p-8 flex flex-col items-center justify-center rounded-lg'
+                style={{ width: '600px', height: '200px' }}
+            >
+                <h2 className="text-white text-6xl text-center">Wallet is offline</h2>
+                <h3 className="text-white text-2xl text-center">Take a coffee</h3>
+            </div>
         </div>
     );
 }
