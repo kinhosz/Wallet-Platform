9a838fdbcfc946f27488b876e339cb7c92f3f7da
kinhosz <jcsc@odoo.com>
2024-08-06 04:25:37 +0200

    [FEAT] Budget: add budget component

--- app/routes/_layout.planning.tsx
+++ app/routes/_layout.planning.tsx
@@ -1,8 +1,23 @@
+import MainButton from "~/components/button";
+import Budget from "~/components/charts/budget";
+
 export default function Planning() {
-    return (
-      <div>
-        Planning
+  return (
+    <div className="m-4 flex flex-row justify-between">
+      <div className="">
+        <div className="text-wallet_orange text-xl text-center">Budget</div>
+        <Budget beginning={400} ending={300} />        
       </div>
-    );
-  }
-  
+      <div className="">
+        div2
+      </div>
+      <div>
+        <MainButton onClick={()=>{}}>Close the month</MainButton>
+        <div className="my-4">
+          <div className="text-md">Current Period</div>
+          <div className="text-sm">June,14 - August,17</div>
+        </div>
+      </div>
+    </div>
+  );
+}
