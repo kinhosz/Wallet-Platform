003b8247445f30b1530af6ce997cae76e02076a4
kinhosz <jcsc@odoo.com>
2024-11-15 23:40:02 +0100

    [FEAT] monetary: add decorate flag

--- app/components/monetary.tsx
+++ app/components/monetary.tsx
@@ -5,16 +5,18 @@ import { getCurrencySymbol } from '../helper/getCurrencySymbol';
 const Monetary: React.FC<{
     className?: string;
     value: number;
+    decorate?: boolean;
 }> = ({
     className,
-    value
+    value,
+    decorate = false
 }) => {
     const negative = Math.sign(value) === -1;
     const finalValue = Math.abs(value).toFixed(2);
     const currency = useCurrency();
 
     return (
-        <div className={`font-sans ${ negative ? 'text-wallet_red': 'text-black'} ${className}`}>
+        <div className={`font-sans ${ decorate ? (negative ? 'text-wallet_red': 'text-green-500') : 'text-black'} ${className}`}>
             {getCurrencySymbol(currency)}{' '}{finalValue}
         </div>
     );
