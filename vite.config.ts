e8a971d0b8d144ee51c8de1b50cbfd806012d63e
kinhosz <jcsc@odoo.com>
2024-12-10 21:00:15 +0100

    [CHORE] remix: remix major changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix({
+    future: {
+      v3_fetcherPersist: true,
+      v3_lazyRouteDiscovery: true,
+      v3_relativeSplatPath: true,
+      v3_singleFetch: true,
+      v3_throwAbortReason: true,
+    },
+  }), tsconfigPaths()],
 });
