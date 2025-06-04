cb4bb779824417346e077da06de3a8a296918e20
kinhosz <jcsc@odoo.com>
2025-06-04 16:12:05 +0200

    [REF] TransactionTable: drop for show
    The transaction table was being too complicated and more
    page related. This commit removes it to render on transactions
    show page, as an Outlet is needed into it.

---
+++ app/services/transactions/RawTransaction.ts
@@ -0,0 +1,9 @@
+export interface RawTransaction {
+    description: string;
+    occurred_at: string;
+    value: string;
+    currency: string;
+    category_name: string;
+    category_icon: number;
+    uuid: string;
+}
